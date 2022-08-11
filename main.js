bb5685d5900bb782145b7509ed9e8a07a4d069d0
Ansh339 <ansh3041.be20@chitkara.edu.in>
2022-08-11 10:12:14 +0530

    script file added in html, main.js working checked

---
+++ main.js
@@ -0,0 +1,5 @@
+const xhr = new XMLHttpRequest();
+const source = 'https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09'
+
+xhr.open('GET',source,false)
+console.log("Working")
