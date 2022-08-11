8ebc0941c3b31171b0e86fa3c8ea98320d3a2fda
Ansh339 <ansh3041.be20@chitkara.edu.in>
2022-08-11 10:23:23 +0530

    data fetched from url and displayed on webpage

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const xhr = new XMLHttpRequest();
 const source = 'https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09'
 
-xhr.open('GET',source,false)
+xhr.open('GET', source, false)
 console.log("Working")
+
+xhr.onreadystatechange = () =>
+{
+    if(xhr.readyState === 4 && xhr.status === 200)
+    {
+        console.log("Connected Successfully");
+
+        const response = JSON.parse(xhr.responseText)
+
+        let output = ''
+
+        for(let i = 0; i < response.length; i++)
+        {
+            output +=  `
+            <p>NAME = ${response[i].name}</p>
+            <p>USERNAME = ${response[i].username}</p>
+            <p>EMAIL = ${response[i].email}</p>
+            <p>CONTACT = ${response[i].phone}</p>
+            <p>WEBSITE = ${response[i].website}</p>
+            `
+        }
+
+        document.querySelector('#detail-container').innerHTML = output
+    }
+}
+
+xhr.send()
