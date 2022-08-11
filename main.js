e485b95ddeb5145f4619d83fa607d6f28a02e7fd
Ansh339 <ansh3041.be20@chitkara.edu.in>
2022-08-11 15:37:13 +0530

    code for displaying data from form moved to main.js, somecomments added in main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// DISPLAY DUMMY LINK DATA
 const xhr = new XMLHttpRequest();
 const source = 'https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09'
 
@@ -67,5 +68,78 @@ xhr.onreadystatechange = () =>
         document.querySelector('#detail-container').innerHTML = output
     }
 }
-
 xhr.send()
+
+// DISPLAY FORM DATA
+var form = document.getElementById('form')
+
+      form.addEventListener('submit', (e) =>
+      {
+        e.preventDefault()
+
+        var name = document.getElementById('name').value
+        var userName = document.getElementById('user-name').value
+        var email = document.getElementById('email').value
+        var street = document.getElementById('street').value
+        var suite = document.getElementById('suite').value
+        var city = document.getElementById('city').value
+        var zipcode = document.getElementById('zipcode').value
+        var lat = document.getElementById('lat').value
+        var lng = document.getElementById('lng').value
+        var contact = document.getElementById('contact').value
+        var website = document.getElementById('website').value
+        var companyName = document.getElementById('company-name').value
+        var catchPhrase = document.getElementById('catch-phrase').value
+        var bs = document.getElementById('bs').value
+        console.log(name)
+
+        let store = ''
+
+        store += `
+        <div class = "post-detail-info">
+          <p>NAME: ${name}</p>
+          <p>USERNAME: ${userName}</p>
+          <p>EMAIL: ${email}</p>
+          <div>
+            <p>ADDRESS</p>
+            <ul>
+              <li>
+                <p>STREET: ${street}</p>
+              </li>
+              <li>
+                <p>SUITE: ${suite}</p>
+              </li>
+              <li>
+                <p>CITY: ${city}</p>
+              </li>
+              <li>
+                <p>ZIPCODE: ${zipcode}</p>
+              </li>
+              <li>
+                <p>LATITUDE: ${lat}</p>
+              </li>
+              <li>
+                <p>LONGITUDE: ${lng}</p>
+              </li>
+            </ul>
+          </div>
+          <p>CONTACT: ${contact}</p>
+          <p>WEBSITE: ${website}</p>
+          <div>
+            <p>COMPANY</p>
+            <ul>
+              <li>
+                <p>NAME: ${companyName}</p>
+              </li>
+              <li>
+                <p>CATCHPHRASE: ${catchPhrase}</p>
+              </li>
+              <li>
+                <p>BS: ${bs}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        `
+        document.querySelector('#data-display').innerHTML = store
+      })
